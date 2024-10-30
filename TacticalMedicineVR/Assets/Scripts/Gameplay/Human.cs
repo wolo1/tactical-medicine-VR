5fe9b31348705200f1f8df3b4b00cbb7f6e71de4
wolo1 <volodya98t@gmail.com>
2024-10-30 15:38:37 +0100

    basic mechanic scene ready

--- TacticalMedicineVR/Assets/Scripts/Gameplay/Human.cs
+++ TacticalMedicineVR/Assets/Scripts/Gameplay/Human.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Human : MonoBehaviour
 {
-    public Texture2D maleJeansCut;
-    public Texture2D maleJeansTourniquet;
-    public GameObject maleClothes;
+    public Texture2D clothesCut;
+    public Texture2D medicineApplied;
+    public GameObject clothes;
     private bool clothesRemoved = false;
 
+    public GameObject bleeding;
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("COLLISION DETECTED");
         if (collision.gameObject.CompareTag("MedicalEquipment"))
         {
             var medicalEquipment = collision.gameObject.GetComponent<MedicalEquipment>();
@@ -19,20 +21,78 @@ public class Human : MonoBehaviour
             {
                 if (medicalEquipment.type == "Scissors")
                 {
-                    maleClothes.GetComponent<Renderer>().material.SetTexture("_BaseMap", maleJeansCut);
+                    clothes.GetComponent<Renderer>().material.SetTexture("_BaseMap", clothesCut);
                     medicalEquipment.audioSource.Play();
                     clothesRemoved = true;
                 }
                 else if (medicalEquipment.type == "Tourniquet" && clothesRemoved)
                 {
-                    maleClothes.GetComponent<Renderer>().material.SetTexture("_BaseMap", maleJeansTourniquet);
+                    clothes.GetComponent<Renderer>().material.SetTexture("_BaseMap", medicineApplied);
+                    medicalEquipment.audioSource.Play();
+                    StopBleeding();
                 }
-
             }
             else
             {
-                Debug.Log("The collided object is not a MedicalEqupment");
+                Debug.Log("The collided object is not a MedicalEquipment");
             }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIGGER ENTER DETECTED");
+        if (other.gameObject.CompareTag("MedicalEquipment"))
+        {
+            var medicalEquipment = other.gameObject.GetComponent<MedicalEquipment>();
+            if (medicalEquipment != null)
+            {
+                if (medicalEquipment.type == "Scissors")
+                {
+                    clothes.GetComponent<Renderer>().material.SetTexture("_BaseMap", clothesCut);
+                    medicalEquipment.audioSource.Play();
+                    clothesRemoved = true;
+                }
+                else if (medicalEquipment.type == "Tourniquet" && clothesRemoved)
+                {
+                    clothes.GetComponent<Renderer>().material.SetTexture("_BaseMap", medicineApplied);
+                    medicalEquipment.audioSource.Play();
+                    StopBleeding();
+                }
+            }
+            else
+            {
+                Debug.Log("The triggered object is not a MedicalEquipment");
+            }
+        }
+    }
+
+    private void StopBleeding()
+    {
+        Debug.Log("StopBleeding method called.");
+
+        // Disable the Animator on the bleeding GameObject
+        var animator = bleeding.GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.enabled = false;
+            Debug.Log("Bleeding animation stopped by disabling the Animator.");
+        }
+        else
+        {
+            Debug.Log("No Animator component found on the bleeding GameObject.");
+        }
+
+        // Disable the Animator on the current GameObject if it exists
+        var currentAnimator = GetComponent<Animator>();
+        if (currentAnimator != null)
+        {
+            currentAnimator.enabled = false;
+            Debug.Log("Animator on the current GameObject disabled.");
+        }
+        else
+        {
+            Debug.Log("No Animator component found on the current GameObject.");
         }
     }
 }
