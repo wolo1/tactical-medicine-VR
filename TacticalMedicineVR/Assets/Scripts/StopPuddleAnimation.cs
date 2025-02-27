33b672b0edd535c1cc5c0f6e8436ec72765adf3f
volo <89660634+wolo1@users.noreply.github.com>
2025-02-27 14:00:00 +0100

    minor scene adjustments

--- TacticalMedicineVR/Assets/Scripts/StopPuddleAnimation.cs
+++ TacticalMedicineVR/Assets/Scripts/StopPuddleAnimation.cs
@@ -9,7 +9,7 @@ public class StopPuddleAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GetComponent<Transform>().localScale.y >= 45)
+        if (GetComponent<Transform>().localScale.y >= 30)
         {
             GetComponent<Animator>().enabled = false;
         }
