a950f3c2243d072a480f724737e8ffe4f7731e77
volo <89660634+wolo1@users.noreply.github.com>
2025-03-04 13:02:10 +0100

    update applyinstructions script

--- TacticalMedicineVR/Assets/ApplyInstructions.cs
+++ TacticalMedicineVR/Assets/ApplyInstructions.cs
@@ -48,6 +48,8 @@ public class ApplyInstructions : MonoBehaviour
             }
             DestroyAllInstructions();
             gameObject.GetComponent<AudioOldMan>().enabled = false;
+            GetComponent<AudioSource>().enabled = false;
+
         }
     }
 
